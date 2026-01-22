5cf27cc5f587db6d9a273146f9780795a1693499
Katherine <kaburbank@gmail.com>
2026-01-22 10:31:12 -0700

    tests written and components modified to pass tests

--- src/components/AccountContainer.jsx
+++ src/components/AccountContainer.jsx
@@ -1,46 +1,46 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 import Sort from "./Sort";
 
 function AccountContainer() {
-  const [transactions,setTransactions] = useState([])
-  const [search,setSearch] = useState("")
-  // console.log(search)
+  const [transactions, setTransactions] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sortBy, setSortBy] = useState("");
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("http://localhost:6001/transactions")
-    .then(r=>r.json())
-    .then(data=>setTransactions(data))
-  },[])
+      .then((r) => r.json())
+      .then((data) => setTransactions(data));
+  }, []);
 
-  function postTransaction(newTransaction){
-    fetch('http://localhost:6001/transactions',{
+  function postTransaction(newTransaction) {
+    fetch("http://localhost:6001/transactions", {
       method: "POST",
-      headers:{
-        "Content-Type": "application/json"
+      headers: {
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(newTransaction)
+      body: JSON.stringify(newTransaction),
     })
-    .then(r=>r.json())
-    .then(data=>setTransactions([...transactions,data]))
-  }
-  
-  // Sort function here
-  function onSort(sortBy){
-    
+      .then((r) => r.json())
+      .then((data) => setTransactions((prev) => [...prev, data]));
   }
 
-  // Filter using search here and pass new variable down
-  
+  function onSort(field) {
+    setSortBy(field);
+  }
+
+  const filteredTransactions = [...transactions]
+    .filter((t) => t.description.toLowerCase().includes(search.toLowerCase()))
+    .sort((a, b) => (sortBy ? a[sortBy].localeCompare(b[sortBy]) : 0));
 
   return (
     <div>
-      <Search setSearch={setSearch}/>
-      <AddTransactionForm postTransaction={postTransaction}/>
-      <Sort onSort={onSort}/>
-      <TransactionsList transactions={transactions} />
+      <Search setSearch={setSearch} />
+      <AddTransactionForm postTransaction={postTransaction} />
+      <Sort onSort={onSort} />
+      <TransactionsList transactions={filteredTransactions} />
     </div>
   );
 }
